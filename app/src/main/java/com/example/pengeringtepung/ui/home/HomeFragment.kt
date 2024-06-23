6872f05c95cf901fe12b17fdd19c079d21cb0840
DWI-PC <dwiseptian2498@gmail.com>
2024-06-23 22:43:02 +0700

    Dwi - 23/06/2024

--- app/src/main/java/com/example/pengeringtepung/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/pengeringtepung/ui/home/HomeFragment.kt
@@ -1,17 +1,26 @@
 package com.example.pengeringtepung.ui.home
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.pengeringtepung.databinding.FragmentHomeBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+    private lateinit var dbRef: DatabaseReference
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -35,6 +44,45 @@ class HomeFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         initView()
+    }
+
+    private fun getData() {
+        dbRef = FirebaseDatabase.getInstance().getReference("pengeringTepung/realteim/temp")
+        dbRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val value = snapshot.value.toString()
+//                Toast.makeText(context, value, Toast.LENGTH_SHORT).show()
+                val debu = binding.tvTemp
+                debu.text = value
+            }
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(context, "Failed To Get Data", Toast.LENGTH_LONG).show()
+            }
+        })
+        dbRef = FirebaseDatabase.getInstance().getReference("pengeringTepung/realtime/hum")
+        dbRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val value = snapshot.value.toString()
+//                Toast.makeText(context, value, Toast.LENGTH_SHORT).show()
+                val debu = binding.tvHum
+                debu.text = value
+            }
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(context, "Failed To Get Data", Toast.LENGTH_LONG).show()
+            }
+        })
+        dbRef = FirebaseDatabase.getInstance().getReference("pengeringTepung/realtime/volt")
+        dbRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val value = snapshot.value.toString()
+//                Toast.makeText(context, value, Toast.LENGTH_SHORT).show()
+                val debu = binding.tvVolt
+                debu.text = value
+            }
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(context, "Failed To Get Data", Toast.LENGTH_LONG).show()
+            }
+        })
     }
 
     private fun initView(){
--- app/src/main/java/com/example/pengeringtepung/ui/pengeringanStart/startPengeringanActivity.kt
+++ app/src/main/java/com/example/pengeringtepung/ui/pengeringanStart/startPengeringanActivity.kt
@@ -4,19 +4,29 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pengeringtepung.databinding.ActivityPengeringanStartBinding
+import com.example.pengeringtepung.ui.home.HomeFragment
 import com.example.pengeringtepung.ui.pengeringanEnd.endPengeringanActivity
+import com.google.firebase.Firebase
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
 
 class startPengeringanActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityPengeringanStartBinding
+
+    private lateinit var database: DatabaseReference
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPengeringanStartBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        database = Firebase.database.reference
+
         binding.btnStart.setOnClickListener {
-            val intent = Intent(this, endPengeringanActivity::class.java)
+            val intent = Intent(this, HomeFragment::class.java)
+            database.child("pengeringTepung").child("/state").setValue("1")
             startActivity(intent)
         }
     }
